96306521038c2e2f36ca6452a620fec64b723f92
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-11 01:18:36 +0600

    1/11/25

--- laptops.py
+++ laptops.py
@@ -1,9 +1,19 @@
 a=[]
 b=[]
-for i in range(int(input())):
+x=int(input())
+for i in range(x):
     m,n=map(int,input().split())
     a.append(m)
     b.append(n)
-a.sort()
-b.sort()
+# a.sort()
+# b.sort()
 
+f=0
+for i in range(x):
+    if a[i]==b[i]:
+        f=1
+        break
+if f:
+    print("Poor Alex")
+else:
+    print("Happy Alex")
