03ffe17537ec21420eab180036c929131a30a004
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-22 20:36:58 +0600

    1/22/25

--- minimal_coprime.py
+++ minimal_coprime.py
@@ -1,14 +1,11 @@
 import sys
 import math
 
-def count_minimal_coprime_segments(l, r):
+t = int(input())
+for _ in range(t):
+    l, r = map(int,input().split())
     count = 0
     for i in range(l, r + 1):
         if math.gcd(i, i) == 1:
             count += 1
-    return count
-
-t = int(sys.stdin.readline().strip())
-for _ in range(t):
-    l, r = map(int, sys.stdin.readline().split())
-    print(r - l + 1 - sum(1 for i in range(l, r + 1) if math.gcd(l, i) != 1))
+    print(count)
